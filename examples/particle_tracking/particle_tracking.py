5e7fd91c3a522c65099f2ca4fc6d12b9000ae8c1
satya-arjunan <satya.arjunan@gmail.com>
2014-12-12 14:52:01 +0900

    Added particle tracking example

--- examples/particle_tracking/particle_tracking.py
+++ examples/particle_tracking/particle_tracking.py
@@ -12,7 +12,6 @@ theSimulator.createEntity('Variable', 'Variable:/:XYPLANE').Value = 5
 theSimulator.createEntity('Variable', 'Variable:/:XZPLANE').Value = 5
 theSimulator.createEntity('Variable', 'Variable:/:YZPLANE').Value = 4
 
-# Create the surface compartment:
 theSimulator.createEntity('System', 'System:/:Surface').StepperID = 'SS'
 theSimulator.createEntity('Variable', 'Variable:/Surface:DIMENSION').Value = 2
 theSimulator.createEntity('Variable', 'Variable:/Surface:VACANT')
@@ -21,19 +20,28 @@ theSimulator.createEntity('Variable', 'Variable:/Surface:As').Value = 0
 theSimulator.createEntity('Variable', 'Variable:/Surface:GFP').Value = 0
 
 logger = theSimulator.createEntity('VisualizationLogProcess', 'Process:/:logger')
-logger.VariableReferenceList = [['_', 'Variable:/Surface:GFP']]
 logger.VariableReferenceList = [['_', 'Variable:/Surface:A']]
 logger.VariableReferenceList = [['_', 'Variable:/Surface:As']]
+logger.VariableReferenceList = [['_', 'Variable:/Surface:GFP']]
 logger.LogInterval = 0.01
 
+#Tag 10 molecules of A with GFP, to get A-GFP. A-GFP can transition to As-GFP:
 tagger = theSimulator.createEntity('TagProcess', 'Process:/:tagger')
 tagger.VariableReferenceList = [['_', 'Variable:/Surface:GFP', '-1' ]]
 tagger.VariableReferenceList = [['_', 'Variable:/Surface:A', '10' ]]
 tagger.VariableReferenceList = [['_', 'Variable:/Surface:As']]
 
 coord = theSimulator.createEntity('CoordinateLogProcess', 'Process:/:coord')
-coord.VariableReferenceList = [['_', 'Variable:/Surface:GFP', '-1' ]]
+coord.VariableReferenceList = [['_', 'Variable:/Surface:A']]
+coord.VariableReferenceList = [['_', 'Variable:/Surface:As']]
+coord.VariableReferenceList = [['_', 'Variable:/Surface:GFP']]
 coord.LogInterval = 0.03
+coord.LogEnd = 10
+
+micro = theSimulator.createEntity('MicroscopyTrackingProcess', 'Process:/:micro')
+micro.VariableReferenceList = [['_', 'Variable:/Surface:GFP', '1']]
+micro.VariableReferenceList = [['_', 'Variable:/Surface:GFP', '-1']]
+micro.ExposureTime = 0.5
 
 populator = theSimulator.createEntity('MoleculePopulateProcess', 'Process:/:pop')
 populator.VariableReferenceList = [['_', 'Variable:/Surface:A']]
