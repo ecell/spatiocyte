c0d0ee636c8d0289a2365754fa3b967ca9c73714
satya-arjunan <satya.arjunan@gmail.com>
2014-12-26 17:16:52 +0900

    Added snrp-particle for multi-algorithm simulation example

--- examples/snrp-particle/snrp-particle.em
+++ examples/snrp-particle/snrp-particle.em
@@ -1,3 +1,9 @@
+#This model performs multi-algorithm simulation using Spatiocyte next reaction
+#method with lattice-based particle reaction-diffusion process.
+#A is a HD species with molecules assumed to be homogeneously distributed in the
+#compartment. B is a individually diffused species by the particle simulator.
+#This model executes the reaction A + B -> C using Spatiocyte next reaction #method.
+
 Stepper SpatiocyteStepper(SS) { 
   VoxelRadius 4.4e-9;
   SearchVacant 0; }
@@ -11,27 +17,26 @@ System System(/) {
   Variable Variable(A) {
       Value 1000;
       Name "HD"; }
-  Variable Variable(B) { Value 2000; }
+  Variable Variable(B) { Value 1500; }
   Variable Variable(C) { Value 0; }
   Process MoleculePopulateProcess(populate) {
     VariableReferenceList [_ Variable:/:B]; }
   Process DiffusionProcess(diffuseB) {
     VariableReferenceList [_ Variable:/:B];
-    D 16e-12; } # m^2/s
+    D 1e-12; } # m^2/s
   Process DiffusionProcess(diffuseC) {
     VariableReferenceList [_ Variable:/:C];
-    D 16e-12; } # m^2/s
+    D 1e-12; } # m^2/s
   Process VisualizationLogProcess(visualize) {
     VariableReferenceList [_ Variable:/:B]
                           [_ Variable:/:C];
-    LogInterval 1e-3; } # s
+    LogInterval 5e-5; } # s
   Process IteratingLogProcess(logiter) {
     VariableReferenceList [_ Variable:/:A]
                           [_ Variable:/:B]
                           [_ Variable:/:C];
-    LogEnd 10;
-    LogStart 0;
-    LogInterval 1e-3; }
+    LogEnd 0.03;
+    LogInterval 1e-4; }
   Process SpatiocyteNextReactionProcess(reaction) {
     VariableReferenceList [_ Variable:/:A -1]
                           [_ Variable:/:B -1]
---
+++ examples/snrp-particle/snrp-particle.py
@@ -0,0 +1,52 @@
+#This model performs multi-algorithm simulation using Spatiocyte next reaction
+#method with lattice-based particle reaction-diffusion process.
+#A is a HD species with molecules assumed to be homogeneously distributed in the
+#compartment. B is a individually diffused species by the particle simulator.
+#This model executes the reaction A + B -> C using Spatiocyte next reaction #method.
+
+sim = theSimulator.createStepper('SpatiocyteStepper', 'SS')
+sim.VoxelRadius = 4.4e-9 
+sim.SearchVacant = 0
+
+theSimulator.rootSystem.StepperID = 'SS'
+theSimulator.createEntity('Variable', 'Variable:/:GEOMETRY').Value = 0
+theSimulator.createEntity('Variable', 'Variable:/:LENGTHX').Value = 5e-7
+theSimulator.createEntity('Variable', 'Variable:/:LENGTHY').Value = 5e-7
+theSimulator.createEntity('Variable', 'Variable:/:LENGTHZ').Value = 5e-7
+theSimulator.createEntity('Variable', 'Variable:/:VACANT')
+A = theSimulator.createEntity('Variable', 'Variable:/:A')
+A.Value = 1000
+A.Name = "HD"
+theSimulator.createEntity('Variable', 'Variable:/:B').Value = 1500
+theSimulator.createEntity('Variable', 'Variable:/:C').Value = 0
+
+pop = theSimulator.createEntity('MoleculePopulateProcess', 'Process:/:pop')
+pop.VariableReferenceList = [['_', 'Variable:/:B']]
+
+dif = theSimulator.createEntity('DiffusionProcess', 'Process:/:diffuseB')
+dif.VariableReferenceList = [['_', 'Variable:/:B']]
+dif.D = 1e-12
+
+dif = theSimulator.createEntity('DiffusionProcess', 'Process:/:diffuseC')
+dif.VariableReferenceList = [['_', 'Variable:/:C']]
+dif.D = 1e-12
+
+log = theSimulator.createEntity('VisualizationLogProcess', 'Process:/:logger')
+log.VariableReferenceList = [['_', 'Variable:/:B']]
+log.VariableReferenceList = [['_', 'Variable:/:C']]
+log.LogInterval = 5e-5
+
+ite = theSimulator.createEntity('IteratingLogProcess', 'Process:/:logiter')
+ite.VariableReferenceList = [['_', 'Variable:/:A']]
+ite.VariableReferenceList = [['_', 'Variable:/:B']]
+ite.VariableReferenceList = [['_', 'Variable:/:C']]
+ite.LogEnd = 0.03
+ite.LogInterval = 1e-4
+
+r = theSimulator.createEntity('SpatiocyteNextReactionProcess', 'Process:/:r')
+r.VariableReferenceList = [['_', 'Variable:/:A', '-1']]
+r.VariableReferenceList = [['_', 'Variable:/:B', '-1']]
+r.VariableReferenceList = [['_', 'Variable:/:C', '1']]
+r.k = 1e-20
+
+run(0.031)
