b19da1c1a0676c6a413380cf6a8fd4d30dd67a8f
Satya Arjunan <satya.arjunan@gmail.com>
2015-01-10 18:38:12 +0900

    Update install-mac

--- install-mac.sh
+++ install-mac.sh
@@ -3,7 +3,7 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew doctor
 brew prune
 brew update
-brew tap homebrew/python homebrew/boneyard
+brew tap homebrew/python
 brew install Caskroom/cask/xquartz
 brew install wget automake autoconf libtool pkg-config gsl pygtk gcc boost-python homebrew/science/hdf5 --with-cxx numpy scipy matplotlib libav glibmm
 wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
@@ -27,7 +27,7 @@ cd wrk
 git clone git://github.com/ecell/spatiocyte
 cd spatiocyte
 patch -N /usr/local/Cellar/glibmm/2.42.0/include/glibmm-2.4/glibmm.h mac_glibmm_h.diff
-brew install gtkglextmm
+brew install homebrew/boneyard/gtkglextmm
 ./autogen.sh
 ./configure --prefix=$HOME/root
 make -j4
