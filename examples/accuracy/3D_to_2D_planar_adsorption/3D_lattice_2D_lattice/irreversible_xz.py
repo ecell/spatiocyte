bfe784fa9c39bb74660f72ada56c466f10bdd3f7
Satya Arjunan <satya.arjunan@gmail.com>
2015-08-25 17:34:03 +0900

    Added model files

--- examples/accuracy/3D_to_2D_planar_adsorption/3D_lattice_2D_lattice/irreversible_xz.py
+++ examples/accuracy/3D_to_2D_planar_adsorption/3D_lattice_2D_lattice/irreversible_xz.py
@@ -42,7 +42,7 @@ logger = theSimulator.createEntity('IteratingLogProcess', 'Process:/:iter')
 logger.VariableReferenceList = [['_', 'Variable:/Surface:A']]
 logger.LogInterval = 1
 logger.LogEnd = 200
-logger.Iterations = 1000
+logger.Iterations = 250
 logger.FileName = "IterateLogXZ.csv"
 
 run(201)
