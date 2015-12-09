abeff2a71e97bc6eba1a76bf1d6c992dcae534e3
satya-arjunan <satya.arjunan@gmail.com>
2015-12-09 13:29:54 +0900

    Added libhdf5-dev in install script

--- install-spatiocyte-ubuntu.sh
+++ install-spatiocyte-ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
-sudo apt-get install git autoconf automake libtool g++ libgsl0-dev python-numpy python-ply python-gtk2 libboost-python-dev libgtkmm-2.4-dev libgtkglextmm-x11-1.2-dev libav-tools blender vlc python-numpy python-scipy python-matplotlib valgrind
+sudo apt-get install git autoconf automake libtool g++ libhdf5-dev libgsl0-dev python-numpy python-ply python-gtk2 libboost-python-dev libgtkmm-2.4-dev libgtkglextmm-x11-1.2-dev libav-tools blender vlc python-numpy python-scipy python-matplotlib valgrind
 echo "export PATH=.:\$HOME/root/bin:\$PATH" >> ~/.bashrc
 echo "export LD_LIBRARY_PATH=\$HOME/root/lib:\$LD_LIBRARY_PATH:." >> ~/.bashrc
 echo "export PYTHONPATH=\$HOME/root/lib/python2.7/site-packages:\$PYTHONPATH" >> ~/.bashrc
