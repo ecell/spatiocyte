05f02f25b31e28b17309a66fc329b43d7cf2e63a
Satya Arjunan <satya.arjunan@gmail.com>
2016-07-06 13:51:19 +0900

    Added particle_tracking example with dimerization tag

---
+++ examples/particle_tracking/particle_tracking_dimer.py
@@ -0,0 +1,71 @@
+sim = theSimulator.createStepper('SpatiocyteStepper', 'SS')
+sim.VoxelRadius = 4.4e-9 
+sim.SearchVacant = 0
+
+theSimulator.rootSystem.StepperID = 'SS'
+theSimulator.createEntity('Variable', 'Variable:/:GEOMETRY').Value = 0
+theSimulator.createEntity('Variable', 'Variable:/:LENGTHX').Value = 1e-8
+theSimulator.createEntity('Variable', 'Variable:/:LENGTHY').Value = 1e-6
+theSimulator.createEntity('Variable', 'Variable:/:LENGTHZ').Value = 1e-6
+theSimulator.createEntity('Variable', 'Variable:/:VACANT')
+theSimulator.createEntity('Variable', 'Variable:/:XYPLANE').Value = 5
+theSimulator.createEntity('Variable', 'Variable:/:XZPLANE').Value = 5
+theSimulator.createEntity('Variable', 'Variable:/:YZPLANE').Value = 4
+
+theSimulator.createEntity('System', 'System:/:Surface').StepperID = 'SS'
+theSimulator.createEntity('Variable', 'Variable:/Surface:DIMENSION').Value = 2
+theSimulator.createEntity('Variable', 'Variable:/Surface:VACANT')
+theSimulator.createEntity('Variable', 'Variable:/Surface:A').Value = 20
+theSimulator.createEntity('Variable', 'Variable:/Surface:As').Value = 0
+theSimulator.createEntity('Variable', 'Variable:/Surface:GFP').Value = 0
+
+logger = theSimulator.createEntity('VisualizationLogProcess', 'Process:/:logger')
+logger.VariableReferenceList = [['_', 'Variable:/Surface:A']]
+logger.VariableReferenceList = [['_', 'Variable:/Surface:As']]
+logger.VariableReferenceList = [['_', 'Variable:/Surface:GFP']]
+logger.LogInterval = 0.01
+
+#Tag 10 molecules of A with GFP, to get A-GFP. A-GFP can transition to As-GFP:
+tagger = theSimulator.createEntity('TagProcess', 'Process:/:tagger')
+tagger.VariableReferenceList = [['_', 'Variable:/Surface:GFP', '-1' ]]
+tagger.VariableReferenceList = [['_', 'Variable:/Surface:A', '10' ]]
+tagger.VariableReferenceList = [['_', 'Variable:/Surface:As']]
+tagger.OligomerSizes = [1,2]
+
+coord = theSimulator.createEntity('CoordinateLogProcess', 'Process:/:coord')
+coord.VariableReferenceList = [['_', 'Variable:/Surface:GFP']]
+coord.LogInterval = 0.01
+coord.LogEnd = 100
+coord.SpeciesID = 1
+coord.MoleculeID = 1
+
+micro = theSimulator.createEntity('MicroscopyTrackingProcess', 'Process:/:micro')
+micro.VariableReferenceList = [['_', 'Variable:/Surface:GFP', '1']]
+micro.VariableReferenceList = [['_', 'Variable:/Surface:GFP', '-1']]
+micro.ExposureTime = 0.5
+
+populator = theSimulator.createEntity('MoleculePopulateProcess', 'Process:/:pop')
+populator.VariableReferenceList = [['_', 'Variable:/Surface:A']]
+populator.VariableReferenceList = [['_', 'Variable:/Surface:As']]
+
+binder = theSimulator.createEntity('DiffusionInfluencedReactionProcess', 'Process:/:reaction1')
+binder.VariableReferenceList = [['_', 'Variable:/Surface:A','-1']]
+binder.VariableReferenceList = [['_', 'Variable:/Surface:A','-1']]
+binder.VariableReferenceList = [['_', 'Variable:/Surface:As','1']]
+binder.p = 0.02
+
+binder = theSimulator.createEntity('SpatiocyteNextReactionProcess', 'Process:/:reaction2')
+binder.VariableReferenceList = [['_', 'Variable:/Surface:As','-1']]
+binder.VariableReferenceList = [['_', 'Variable:/Surface:A','1']]
+binder.VariableReferenceList = [['_', 'Variable:/Surface:A','1']]
+binder.k = 0.3
+
+diffuser = theSimulator.createEntity('DiffusionProcess', 'Process:/:diffuseA')
+diffuser.VariableReferenceList = [['_', 'Variable:/Surface:A']]
+diffuser.D = 1e-13
+
+diffuser = theSimulator.createEntity('DiffusionProcess', 'Process:/:diffuseAs')
+diffuser.VariableReferenceList = [['_', 'Variable:/Surface:As']]
+diffuser.D = 1e-15
+
+run(100)
